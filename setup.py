834dc30bfe2ccc35d0a0870b6b29ffb6d403fab8
Toby Marshall <toby.marshall@global.com>
2022-02-01 14:02:40 +0000

    fix: updated the click package version v2

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
   install_requires=[
     'Beaker>=1.11.0,<2',
     'celery>=5.1.2,<5.2.3',
-    'click>=8.0.0,<9.0',
+    'click>=7.1.2,<8.0',
     'cryptography>=2.9.2<3',
     'github3.py>=1.3.0,<2',
     'github3apps.py>=0.2.2,<0.3',
