f18d7950d83d9b87edd431cabbaa4da3f86e5c4b
Toby Marshall <toby.marshall@global.com>
2022-02-01 13:34:21 +0000

    fix: updated the pyjwt dependency

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     'cryptography>=2.9.2<3',
     'github3.py>=1.3.0,<2',
     'github3apps.py>=0.2.2,<0.3',
-    'pyjwt>=2.0.0,<3',
+    'pyjwt>=1.5.3,<2',
     'PyYAML>=6.0,<7',
     'fastapi>=0.73.0',
   ],
